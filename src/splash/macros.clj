42de57fae1e4448c0cd4e0ad7fe37211a42c6f85
Crispin Wellington <retrogradeorbit@gmail.com>
2015-08-18 19:07:00 +0800

    start playing with multibind macro

--- src/splash/macros.clj
+++ src/splash/macros.clj
@@ -12,3 +12,14 @@
      (.addChild (-> ~canvas :layer ~layer) ~symb)
      (do ~@body)
      (.removeChild (-> ~canvas :layer ~layer) ~symb)))
+
+;; todo: reqrite these two macros recursively
+(defmacro with-sprites [canvas layer binds & body]
+  (let [symbols (map first (partition 2 binds))]
+    (list 'let binds
+          (concat (map (fn [symb] `(.addChild (-> ~canvas :layer ~layer) ~symb)) symbols)
+                  [(cons 'do body)]
+                  (map (fn [symb] `(.removeChild (-> ~canvas :layer ~layer) ~symb)) (reverse symbols))))))
+
+
+(macroexpand '(with-sprites canv lay [a my-a b my-b] (do1) (do2)))
